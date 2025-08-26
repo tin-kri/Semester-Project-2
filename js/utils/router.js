00e53ecd5f8acb6ca357c8724f298cbcca169b79
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-08-26 13:04:42 +0200

    added profile redirect

--- js/utils/router.js
+++ js/utils/router.js
@@ -1,35 +1,44 @@
 export function initializeApp() {
-    const currentPath = window.location.pathname;
-    console.log('Current path:', currentPath);
-    
-    switch (true) {
-        case currentPath.includes('/auth/register'):
-            initializeRegisterPage();
-            break;
-        case currentPath.includes('/auth/login'):
-            initializeLoginPage();
-            break;
-        case currentPath === '/' || currentPath.includes('index'):
-            initializeLandingPage();
-            break;
-        default:
-            console.log('Unknown route:', currentPath);
-    }
+  const currentPath = window.location.pathname;
+  console.log("Current path:", currentPath);
+
+  switch (true) {
+    case currentPath.includes("/auth/register"):
+      initializeRegisterPage();
+      break;
+    case currentPath.includes("/auth/login"):
+      initializeLoginPage();
+      break;
+    case currentPath.includes("/profile"):
+      initializeProfilePage();
+      break;
+    case currentPath === "/" || currentPath.includes("index"):
+      initializeLandingPage();
+      break;
+    default:
+      console.log("Unknown route:", currentPath);
+  }
 }
 async function initializeRegisterPage() {
-    console.log('Loading register page...');
-    const { initRegister } = await import('../pages/register.js');
-    initRegister();
+  console.log("Loading register page...");
+  const { initRegister } = await import("../pages/register.js");
+  initRegister();
 }
 
 async function initializeLoginPage() {
-    console.log('Loading login page...');
-    const { initLogin } = await import('../pages/login.js');
-    initLogin();
+  console.log("Loading login page...");
+  const { initLogin } = await import("../pages/login.js");
+  initLogin();
+}
+
+async function initializeProfilePage() {
+    console.log('Loading profile page...');
+    const { initProfile } = await import('../pages/profile.js');
+    initProfile();
 }
 
 async function initializeLandingPage() {
-    console.log('Loading landing page...');
-    const { initLandingPage } = await import('../pages/landingPage.js');
-    initLandingPage();
+  console.log("Loading landing page...");
+  const { initLandingPage } = await import("../pages/landingPage.js");
+  initLandingPage();
 }
