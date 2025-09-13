ee6b584871a9aecb44acf82fe50c286e226e52e3
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-09-13 19:29:42 +0200

    playwright config added and mobile nav test

--- tests/mobile-navigation.spec.js
+++ tests/mobile-navigation.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Mobile Navigation', () => {
   test('mobile menu shows and hides when clicked', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
+    await page.goto('/');
 
     // Set mobile viewport
     await page.setViewportSize({ width: 390, height: 844 });
@@ -13,31 +13,31 @@ test.describe('Mobile Navigation', () => {
 
     // Mobile menu should be hidden initially
     const mobileMenu = page.locator('#mobile-menu');
-    await expect(mobileMenu).toHaveClass(/hidden/);
+    await expect(mobileMenu).not.toBeVisible();
 
     // Click to open menu
     await menuButton.click();
 
-    // Menu should no longer have 'hidden' class
-    await expect(mobileMenu).not.toHaveClass(/hidden/);
+    await expect(mobileMenu).toBeVisible();
 
     // Click again to close menu
     await menuButton.click();
 
-    // Menu should be hidden again
-    await expect(mobileMenu).toHaveClass(/hidden/);
+    await expect(mobileMenu).not.toBeVisible();
   });
 
   test('mobile menu navigation links work', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
+    await page.goto('/');
     await page.setViewportSize({ width: 390, height: 844 });
 
     // Open mobile menu
     const menuButton = page.locator('button[data-mobile-menu]');
     await menuButton.click();
 
-    // Check navigation links are visible
     const mobileMenu = page.locator('#mobile-menu');
+    await expect(mobileMenu).toBeVisible();
+
+    // Check navigation links are visible
     await expect(mobileMenu.locator('text=Auctions')).toBeVisible();
     await expect(mobileMenu.locator('text=Sell')).toBeVisible();
   });
