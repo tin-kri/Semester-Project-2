c252e8a4d67cb5dd1bee01ad26bca8be9cc8345f
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-09-13 13:44:09 +0200

    trying to solve deploy issue with paths in defineconfig.

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,22 @@ export default defineConfig({
   css: {
     postcss: './postcss.config.js',
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        login: './auth/login/index.html',
+        register: './auth/register/index.html',
+        browseListings: './browse-listings/index.html',
+        createListing: './create-listing/index.html',
+        listingDetails: './listing-details/index.html',
+        profile: './profile/index.html',
+      },
+    },
+  },
   test: {
-    environment: 'happy-dom', // or 'jsdom'
+    environment: 'happy-dom',
     globals: true,
-    setupFiles: [], // add test setup files if needed
+    setupFiles: [],
   },
 });
